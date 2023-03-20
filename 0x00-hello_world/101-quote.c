6759466cc3e175fe8db00fa45b887162cb2a24b2
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-03-20 06:18:44 -0700

    Final task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - main function
  *
- * Return: always 0
+ * Return: always 1
  */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
