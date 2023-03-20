fb4a7f683b4dbc2d46d1bda8ca4e9eba2727798c
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-03-20 05:52:27 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
- * main - this is the main function 
- *
- * Return: returns 0 always
- */
 
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
