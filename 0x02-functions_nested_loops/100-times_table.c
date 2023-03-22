2dae8d95d4b7c3df211f912fe5411dd8cd5d8d4c
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-03-21 17:01:17 -0700

    task 13

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,36 +6,35 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		return;
 
-	int i, j, product;
+	int num, mult, prod;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (num = 0; num <= n; num++)
 		{
-			product = i * j;
-			if (j == 0)
-				_putchar('0');
-			else
-				_putchar(' ');
-			if (product < 10)
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (product < 100)
-			{
-				_putchar(' ');
-			}
-			_putchar(product / 100 % 10 + '0');
-			_putchar(product / 10 % 10 + '0');
-			_putcharutchar(product % 10 + '0');
-			if (j < n)
 				_putchar(',');
-			else
-				_putchar('\n');
+				_putchar(' ');
+				prod = num * mult;
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+			}
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 0; i < 102424; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
