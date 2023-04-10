290bd2c010a8321988ba041c4a15de007653a5aa
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-04-10 14:58:53 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: parameter
+ * @agrv: the parameter
+ * Return: will be 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
