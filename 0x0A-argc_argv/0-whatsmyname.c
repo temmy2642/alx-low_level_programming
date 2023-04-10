a85cb221d02da3a59913ac45a93c864fd6d2fd5f
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-04-10 15:28:58 -0700

    task 0 correction

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * @agrv: the parameter
  * Return: will be 0
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
