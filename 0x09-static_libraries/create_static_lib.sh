987f0289c63f183cc6183d3b2c882f3ac77a82b3
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-04-10 18:32:04 -0700

    corrections

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Compile all the .c files into .o files
 gcc -c *.c
-ar rc liball.a *.o
+
+# Create the static library
+ar -rc liball.a *.o
+
+# Index the library
 ranlib liball.a
 
+# Clean up the .o files
+rm *.o
