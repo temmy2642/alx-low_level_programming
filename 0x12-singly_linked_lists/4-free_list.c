032db6fdb25250c3b78f5c7bba561f85f80ae929
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-04-27 21:29:14 -0700

    mandatory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+* free_list - frees a list_t list
+* @head: pointer to the head of the list
+*
+* Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
+
