8b8be217f31f898e8f7f6e0ea786a4eccc3ab692
Olaniyan Temidayo Abraham <dayotemi90@gmail.com>
2023-04-27 21:56:54 -0700

    updated

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * print_before_main - Prints a message before the main function is executed.
- *
+ * Description: prints the message
  *
  * Return: This function does not return anything.
  */
 
-void __attribute__ ((constructor)) before_main()
+void __attribute__ ((constructor)) print_before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,17 @@
-section .data
-  hello db 'Hello, Holberton', 10, 0
+section .rodata
+        str1 db "Hello, Holberton",0x0a,0
 
 section .text
-  global main
-  extern printf
+        global _start
 
-main:
-  push rbp
-  mov rbp, rsp
+_start:
+        push rbp
+        mov rbp, rsp
 
-  push qword hello
-  call printf
-  add rsp, 8
+        mov edi, str1
+        xor eax, eax
+        call printf
 
-  mov eax, 0
-  pop rbp
-  ret
+        mov eax, 0
+        pop rbp
+        ret
